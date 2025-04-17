7ac21867ff11b58e1b671a3062a870ba9f59835b
Joe <joebbaney@gmail.com>
2025-04-17 13:29:43 -0500

    finished exercises 1-8

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let newStr = ''
+      for (let i = 0; i < num; i++) {
+        newStr += str
+      }
+    return newStr
+  };
 
-};
+repeatString('cat', 3)
 
 // Do not edit below this line
 module.exports = repeatString;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0
 
+    if (typeof a !== 'number' || typeof b !== 'number') return 'ERROR'
+    if (Number.isInteger(a) !== true || Number.isInteger(b) !== true) return 'ERROR'
+    if (a < 0 || b < 0) return 'ERROR'
+
+    const [start, end] = a < b ? [a, b] : [b, a]
+
+    for (let i = start; i <= end; i++) {
+        sum += i
+    }
+
+    return sum
 };
 
 // Do not edit below this line
