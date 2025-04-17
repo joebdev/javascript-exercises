f246fe83853392d360cef3057ffa98864c3621bb
Joe <joebbaney@gmail.com>
2025-04-17 13:28:57 -0500

    finished problems 1 - 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if (arr.length == 0) return 0
+  if (arr.length == 1) return arr[0]
+
+  let sum = 0
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  }
+
+  return sum
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let total = 1
 
+  for (let i = 0; i < arr.length; i++) {
+    total *= arr[i]
+  }
+
+  return total
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a == 0) return 1
+  
+  let factoredNum = 1
+
+  for (let i = 1; i <= a; i++) {
+    factoredNum *= i
+  }
+
+  return factoredNum
 };
 
 // Do not edit below this line
